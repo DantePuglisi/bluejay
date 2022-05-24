b27ef251481afe06462a53645b0007bb79c8e296
Dante P <dantejpuglisi@gmail.com>
2022-05-24 18:02:29 -0300

    Update Peripheral.swift

--- Bluejay/Bluejay/Peripheral.swift
+++ Bluejay/Bluejay/Peripheral.swift
@@ -56,6 +56,11 @@ class Peripheral: NSObject {
     public var identifier: PeripheralIdentifier {
         return PeripheralIdentifier(uuid: cbPeripheral.identifier, name: cbPeripheral.name)
     }
+    
+    /// This peripheral's discovered services.
+    public var services: [CBService]? {
+        return cbPeripheral.services
+    }
 
     // MARK: - Operations
 
