43bd298b82bd435c04e46d9b396b0f5f9474735e
Dante P <dantejpuglisi@gmail.com>
2022-05-24 18:20:18 -0300

    Revert "Update Bluejay.swift"
    This reverts commit 83a65a0c4af0af2b24c730d95966803143aca037.

--- Bluejay/Bluejay/Bluejay.swift
+++ Bluejay/Bluejay/Bluejay.swift
@@ -41,7 +41,7 @@ public class Bluejay: NSObject { //swiftlint:disable:this type_body_length
     private var connectingPeripheral: Peripheral?
 
     /// Reference to a peripheral that is connected. If this is nil, then the peripheral should either be disconnected or still connecting. This is used to help determine the state of the peripheral's connection.
-    var connectedPeripheral: Peripheral?
+    private var connectedPeripheral: Peripheral?
 
     /// The previous connection timeout used.
     private var previousConnectionTimeout: Timeout?
